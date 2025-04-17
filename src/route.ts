7d4cef197104b718a017a34bdf8de46e28ea1617
drumil32 <akheniad@gmail.com>
2025-04-17 22:48:05 +0530

    [route update]: routing added for get marketing log

--- src/route.ts
+++ src/route.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { COLLECTION_NAMES } from './config/constants';
-import { createLeadLog } from './controllers/marketingController';
+import { createLeadLog, getLeadLog } from './controllers/marketingController';
 export const apiRouter = express.Router();
 
 apiRouter.post(`/marketing/${COLLECTION_NAMES.LEAD}`, createLeadLog);
 
+apiRouter.get(`/marketing/${COLLECTION_NAMES.LEAD}/:documentId`, getLeadLog);
